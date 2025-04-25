e00a19b4f0cfc51300d20f853a4c79a46b5e85e9
Raki Rahman <mdrakiburrahman@gmail.com>
2025-04-25 07:34:51 -0400

    Done!

--- README.md
+++ README.md
@@ -77,7 +77,11 @@ go run main.go
 ```bash
 cd cert-auth-go
 
-go run main.go --cert-abs-path "${GIT_ROOT}/cert-auth-go/.secrets/myCert.cer"
+go run main.go \
+   --cert-abs-path "${GIT_ROOT}/cert-auth-go/.secrets/myCert.cer" \
+   --tenant-id "72f988bf-86f1-41af-91ab-2d7cd011db47" \
+   --client-id "8b6e7cc1-d791-4844-bac4-cd50d649e63d" \
+   --scope "https://management.core.windows.net/.default"
 ```
 
 ## OpenTelemetry
--- cert-auth-go/main.go
+++ cert-auth-go/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"context"
 	"crypto/rsa"
+	"crypto/x509"
 	"fmt"
 	"os"
+	"time"
 
 	"cert-auth-go/utils" // Import the utils package
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/spf13/cobra"
 )
 
 func main() {
-	var certAbsPath string
+	var certAbsPath, tenantID, clientID, scope string
 
 	var rootCmd = &cobra.Command{
 		Use:   "cert-auth-go",
@@ -21,10 +26,26 @@ func main() {
 				fmt.Println("Error: --cert-abs-path is required")
 				os.Exit(1)
 			}
+			if tenantID == "" {
+				fmt.Println("Error: --tenant-id is required")
+				os.Exit(1)
+			}
+			if clientID == "" {
+				fmt.Println("Error: --client-id is required")
+				os.Exit(1)
+			}
+			if scope == "" {
+				fmt.Println("Error: --scope is required")
+				os.Exit(1)
+			}
 
 			certChain, err := utils.GetCertChainByFilePath(certAbsPath)
-			cert, rsaPrivateKey, err := utils.GetCertByFilePath(certAbsPath)
+			if err != nil {
+				fmt.Printf("Failed to get certificate chain: %v\n", err)
+				os.Exit(1)
+			}
 
+			cert, rsaPrivateKey, err := utils.GetCertByFilePath(certAbsPath)
 			if err != nil {
 				fmt.Printf("Failed to get certificate chain: %v\n", err)
 				os.Exit(1)
@@ -47,10 +68,37 @@ func main() {
 			}
 			fmt.Printf("  RSA Private Key Length: %d bits\n", rsaKey.Size()*8)
 			fmt.Println("Certificate and RSA Private Key retrieved successfully.")
+
+			cred, err := azidentity.NewClientCertificateCredential(
+				tenantID,
+				clientID,
+				[]*x509.Certificate{cert},
+				rsaPrivateKey,
+				&azidentity.ClientCertificateCredentialOptions{ClientOptions: policy.ClientOptions{}, DisableInstanceDiscovery: true, SendCertificateChain: true},
+			)
+			if err != nil {
+				fmt.Printf("Failed to create ClientCertificateCredential: %v\n", err)
+				os.Exit(1)
+			}
+			fmt.Println("ClientCertificateCredential created successfully.")
+
+			token, err := cred.GetToken(context.Background(), policy.TokenRequestOptions{
+				Scopes: []string{scope},
+			})
+			if err != nil {
+				fmt.Printf("Failed to get token: %v\n", err)
+				os.Exit(1)
+			}
+			fmt.Printf("Access Token: %s\n", token.Token)
+			fmt.Printf("Expires On: %s\n", token.ExpiresOn.Format(time.RFC3339))
+			fmt.Println("Token retrieved successfully.")
 		},
 	}
 
 	rootCmd.Flags().StringVar(&certAbsPath, "cert-abs-path", "", "Absolute path to the certificate file")
+	rootCmd.Flags().StringVar(&tenantID, "tenant-id", "", "Azure Tenant ID")
+	rootCmd.Flags().StringVar(&clientID, "client-id", "", "Azure Client ID")
+	rootCmd.Flags().StringVar(&scope, "scope", "https://management.core.windows.net/.default", "Azure scope for token")
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
