0a3c8d039b433f0464b685c447ca2d4689a83bdf
Raki Rahman <mdrakiburrahman@gmail.com>
2026-05-06 15:55:44 -0400

    Up

--- contrib/bootstrap-dev-env.sh
+++ contrib/bootstrap-dev-env.sh
@@ -26,6 +26,29 @@ if [ ! -z "$PACKAGES" ]; then
     sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $PACKAGES 2>&1 > /dev/null
 fi
 
+DOCKER_VERSION="5:27.5.1-1~ubuntu.24.04~noble"
+
+if ! [ -x "$(command -v docker)" ]; then
+  echo "docker is not installed on your devbox, installing..."
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update -q
+  sudo apt-get install -y apt-transport-https ca-certificates curl
+  sudo apt-get install -y --allow-downgrades docker-ce="$DOCKER_VERSION" docker-ce-cli="$DOCKER_VERSION" containerd.io
+else
+  echo "docker is already installed."
+fi
+
+sudo mkdir -p /etc/docker
+echo '{"max-concurrent-downloads": 32}' | sudo tee /etc/docker/daemon.json > /dev/null
+
+echo "docker is installed, restarting..."
+sudo systemctl restart docker
+
+sudo chmod 666 /var/run/docker.sock
+docker container ls
+docker ps -q | xargs -r docker kill
+
 echo ""
 echo "┌────────────────────────────────────┐"
 echo "│ Checking for language dependencies │"
@@ -55,15 +78,21 @@ echo "│ Checking for CLI tools │"
 echo "└────────────────────────┘"
 echo ""
 
-if ! command -v docker &> /dev/null; then
-    echo "docker not found - installing..."
-    curl -sL https://get.docker.com | sudo bash
+export PATH=$(echo "$PATH" | tr ':' '\n' | grep -v "/mnt/c" | tr '\n' ':' | sed 's/:$//')
+AZ_PATH=$(which az 2>/dev/null)
+if [[ -z "$AZ_PATH" || "$AZ_PATH" == *"/mnt/c"* ]]; then
+  echo "Native Linux Azure CLI not found, installing..."
+  curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+  export PATH="$HOME/bin:$PATH"
+  [[ -f "$HOME/.bashrc" ]] && source "$HOME/.bashrc"
+else
+  echo "Native Linux Azure CLI already installed at: $AZ_PATH"
 fi
-sudo chmod 666 /var/run/docker.sock
 
-if ! command -v az &> /dev/null; then
-    echo "az not found - installing..."
-    curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+az account get-access-token --query "expiresOn" -o tsv >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    echo "az is not logged in, logging in..."
+    az login >/dev/null
 fi
 
 if ! command -v duckdb &> /dev/null; then
@@ -78,8 +107,6 @@ echo "│ Installing VS Code extensions │"
 echo "└───────────────────────────────┘"
 echo ""
 
-code --install-extension github.copilot
-code --install-extension eamodio.gitlens
 code --install-extension golang.go@0.45.0
 code --install-extension ms-vscode.makefile-tools
 
